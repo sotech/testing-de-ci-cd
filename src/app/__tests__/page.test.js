d797a5fe83fa59494815eff36a0eff9f7c0446f7
German Sommariva <germansommariva@gmail.com>
2024-11-18 19:25:14 -0300

    agregado usuarios tests

--- src/app/__tests__/page.test.js
+++ src/app/__tests__/page.test.js
@@ -1,13 +1,24 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import HomeData from '../home';
- 
+
 describe('Home', () => {
   it('renders a heading', () => {
     render(<HomeData />)
- 
+
     const heading = screen.getByRole('heading', { level: 1 })
- 
+
     expect(heading).toBeInTheDocument()
+  })
+})
+
+describe("Usuarios", () => {
+  it('verifica cantidad de cursos de un usuario', () => {
+    const usuario = {};
+    const curso1 = { nombre: 'Cursito 1' };
+    usuario.cursos = [];
+    usuario.cursos.push(curso1);
+
+    expect(usuario.cursos.length).toBeGreaterThanOrEqual(1);
   })
 })
